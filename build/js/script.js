fb6d58163d2ef7f3df2057dd94901f610a291b92
TatianaBz <64534286+TatianaBz@users.noreply.github.com>
2021-09-21 22:56:29 +0700

    Delete build directory

--- build/js/script.js
+++
@@ -1,6 +0,0 @@
-let button = document.querySelector('.sait-nav__button');
-let elem = document.querySelector('.myDropdown');
-
-button.addEventListener('click', function() {
-	elem.classList.toggle('opened');
-});
