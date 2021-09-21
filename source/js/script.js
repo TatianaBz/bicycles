762b43032c567c6ccd83f1f21e996479c415a08a
TatianaBz <64534286+TatianaBz@users.noreply.github.com>
2021-09-21 22:56:56 +0700

    Delete source directory

--- source/js/script.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.navigation__button');
-let elem = document.querySelector('.header__nav');
-let menuClose = document.querySelector('.navigation__closed');
-
-button.addEventListener('click', function() {
-	elem.classList.toggle('header-nav--opened');
-  menuClose.classList.toggle('close');
-});
